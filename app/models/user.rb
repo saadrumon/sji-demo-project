347479d9f4c25d82b5c08ef152f62e7a942cedda
aranya059 <aranya.riaz@gmail.com>
2022-07-29 16:27:29 +0600

    Logo image change and validation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
+  validates :first_name, presence: true, length: { maximum: 250 }
+  validates :last_name, presence: true, length: { maximum: 250 }
+  validates :payment_method, presence: true
+
   BANK_ACCOUNT = 0
   CARD = 1
 
