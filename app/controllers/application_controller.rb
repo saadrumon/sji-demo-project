bb9ef2b9432cc55b55cb17f904104b29683e9c98
saadrumon <saadrumon@gmail.com>
2022-07-29 15:42:45 +0600

    Add columns to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer
+      .permit(:sign_in) do |u|
+      u.permit(
+        :email,
+        :password,
+        :remember_me
+      )
+    end
+
+    devise_parameter_sanitizer
+      .permit(:account_update) do |u|
+      u.permit(
+        :first_name,
+        :last_name,
+        :payment_method,
+        :email,
+        :password,
+        :password_confirmation
+      )
+    end
+  end
 end
