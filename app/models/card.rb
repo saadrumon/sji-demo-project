feda0e46cec250189748b973e6ee4c62f1945d61
aranya059 <aranya.riaz@gmail.com>
2022-07-29 18:37:53 +0600

    Card added

---
+++ app/models/card.rb
@@ -0,0 +1,19 @@
+class Card < ApplicationRecord
+  validates :card_number,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 16 }
+  validates :card_holder_name, presence: true, length: { maximum: 250 }
+  validates :card_type, presence: true
+  validates :expiration_date, presence: true
+
+  MASTERCARD = 0
+  VISA_CARD = 1
+  AMERICAN_EXPRESS = 2
+
+  enum card_type: {
+    Mastercard: MASTERCARD,
+    Visa_card: VISA_CARD,
+    American_express: AMERICAN_EXPRESS
+  }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'cards/new'
+  get 'cards/create'
+  get 'cards/edit'
+  get 'cards/update'
+  get 'cards/destroy'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "homes#index"
   resources :bank_accounts, only: %i[new create edit update destroy]
+  resources :cards, only: %i[new create edit update destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
