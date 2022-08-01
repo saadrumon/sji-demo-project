4014111601759f605be839f12e04bffbf77fb884
aranya059 <aranya.riaz@gmail.com>
2022-08-01 14:39:57 +0600

    Add purchase adjustment logic

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :set_purchase, only: %i[edit update]
   def index
     if current_user.is_admin
       @purchases = Purchase.all
@@ -12,11 +13,40 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    @current_user = current_user
+    @purchase.amount = params[:purchase][:volume].to_f * ENV["FUEL_COST_PER_VOLUME"].to_f
+    @purchase.user_id = @current_user.id
+    if params[:purchase][:payment_type].eql?('Pay_now')
+      @purchase.status = Purchase::PENDING
+    else
+      @purchase.status = Purchase::UNPAID
+    end
+    if @purchase.save
+      redirect_to purchases_path,
+                  notice: 'Purchase fuel successfully'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @purchase.adjust_amount(params[:purchase][:adjustment_amount].to_f)
+    redirect_to purchases_path,
+                notice: 'Adjustment successfully'
+  end
+
+  private
+  def set_purchase
+    @purchase = Purchase.find(params[:id])
+  end
+  def purchase_params
+    params.require(:purchase).permit(
+                                 :volume,
+                                 :payment_type,
+                                 :adjustment_amount)
   end
 end
--- db/migrate/20220731221258_create_purchases.rb
+++ db/migrate/20220731221258_create_purchases.rb
@@ -2,10 +2,11 @@ class CreatePurchases < ActiveRecord::Migration[6.1]
   def change
     create_table :purchases do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :volume
-      t.integer :amount
+      t.float :volume
+      t.float :amount
       t.integer :status
-      t.integer :type
+      t.integer :payment_type
+      t.float :adjustment_amount
 
       t.timestamps
     end
